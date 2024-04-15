666b35f3d99a1e399b7414dffbaa25386167995d
Angel Gonzalez <angeledugo@gmail.com>
2024-04-15 13:53:09 -0500

    Carga del Proyecto Config

---
+++ src/main/java/com/microservises/config/MicroservisesConfigApplication.java
@@ -0,0 +1,13 @@
+package com.microservises.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservisesConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservisesConfigApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/microservises/config/MicroservisesConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservises.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservisesConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
